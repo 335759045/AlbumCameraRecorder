9613702205b9d3f4a1831dbbee03af9c0f29b9bf
zhongjh <254191389@qq.com>
2021-11-10 11:07:19 +0800

    开始转换kotlin

---
+++ albumCameraRecorderCommonKotlin/src/main/java/com/zhongjh/albumcamerarecordercommonkotlin/widget/IncapableDialog.kt
@@ -0,0 +1,14 @@
+package com.zhongjh.albumcamerarecordercommonkotlin.widget
+
+import androidx.fragment.app.DialogFragment
+
+/**
+ * 公共弹窗
+ * @author zhongjh
+ * @date 2021/11/10
+ */
+class IncapableDialog() : DialogFragment() {
+
+
+
+}
